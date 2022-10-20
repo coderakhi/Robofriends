bbd1d8ac44a4b1d2403009c9f27f28061474fc0e
Akhil Nair <AKNair@ap.linedata.com>
2022-10-20 09:53:48 +0530

    Updates

---
+++ precache-manifest.2113e4166ea35674e63e4111046b3639.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dbbcc7e7dae7122c9f8",
+    "url": "/Robofriends/static/css/main.12a6f4da.chunk.css"
+  },
+  {
+    "revision": "5dbbcc7e7dae7122c9f8",
+    "url": "/Robofriends/static/js/main.5dbbcc7e.chunk.js"
+  },
+  {
+    "revision": "95c7267b3dea30deff0e",
+    "url": "/Robofriends/static/js/1.95c7267b.chunk.js"
+  },
+  {
+    "revision": "84983af66aca60bf24a4",
+    "url": "/Robofriends/static/css/2.514201be.chunk.css"
+  },
+  {
+    "revision": "84983af66aca60bf24a4",
+    "url": "/Robofriends/static/js/2.84983af6.chunk.js"
+  },
+  {
+    "revision": "d56a16e77dd84075f9ab",
+    "url": "/Robofriends/static/js/runtime~main.d56a16e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3e5154552ea2519f10652755deab7219",
+    "url": "/Robofriends/index.html"
+  }
+];
